10f5e06a1eba42bbce46b733670fe8ac98c2e4aa
Nashid K <kp.nashid.nashid@gmail.com>
2024-01-13 14:02:40 +0530

    an event added for each choice

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let userScore = 0;
 let compScore = 0;
 
-const choices = document.querySelectorAll(".choices");
+const choices = document.querySelectorAll(".choice");
 
 choices.forEach((choice) => {
     console.log(choice);
     choice.addEventListener("click", () => {
-        console.log("choice was clicked");
+        const userChoice = choice.getAttribute("id");
+        console.log("choice was clicked", userChoice);
     });
 });
