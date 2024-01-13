dc8cf35fa12630b373fb964dbbf7466e5c6ff672
Nashid K <kp.nashid.nashid@gmail.com>
2024-01-13 18:22:55 +0530

    user choice, compouter choice and play game functioins created

--- app.js
+++ app.js
@@ -3,10 +3,35 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];    
+};
+
+const drawGame = () => {
+    console.log("game was draw");
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+    // generate computer choice
+        const compChoice = genCompChoice();
+        console.log("comp choice = ", compChoice);  
+
+        if (userChoice === compChoice){
+         drawGame();
+        }
+    };
+
+
+// Add a click event listener to each element in the 'choices' collection
 choices.forEach((choice) => {
-    console.log(choice);
+    // When a choice is clicked, execute the following function
     choice.addEventListener("click", () => {
+        // Retrieve the 'id' attribute of the clicked choice, representing the user's selection
         const userChoice = choice.getAttribute("id");
-        console.log("choice was clicked", userChoice);
+        // Call the 'playGame' function with the user's choice as an argument
+        playGame(userChoice);
     });
 });
